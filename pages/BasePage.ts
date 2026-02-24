fb7c0314be4f20405bb79b80f59fa305f8b49a9b
Anca Nechita <anca.nechita.moldavia@gmail.com>
2026-02-24 12:49:39 +0200

    Refactor BasePage structure

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -1,11 +1,14 @@
 import { Page } from "@playwright/test";
 
 export class BasePage {
-  readonly page: Page;
+  protected readonly page: Page;
   constructor(page: Page) {
     this.page = page;
   }
   async navigateTo(url: string) {
     await this.page.goto("url");
+  }
+  async getTitle() {
+    return this.page.title();
   }
 }
