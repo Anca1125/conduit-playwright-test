b98126f0b8b16af34c441020e1bf8e5fa661fac8
Anca Nechita <anca.nechita.moldavia@gmail.com>
2026-02-26 08:12:42 +0200

    Add article tests with POMs

--- pages/new-article-page.ts
+++ pages/new-article-page.ts
@@ -6,13 +6,15 @@ export class NewArticlePage extends BasePage {
   readonly deleteArticle: Locator;
   readonly writeAComment: Locator;
   readonly postAcomment: Locator;
+  readonly likeTheArticle: Locator;
 
   constructor(page: Page) {
     super(page);
-    this.editArticle = page.locator(".ion-edit");
-    this.deleteArticle = page.locator(".ion-trash-a");
+    this.editArticle = page.locator(".ion-edit").first();
+    this.deleteArticle = page.locator(".ion-trash-a").first();
     this.writeAComment = page.locator('[placeholder="Write a comment..."]');
     this.postAcomment = page.locator('[type="submit"]');
+    this.likeTheArticle = page.locator(".ion-heart").first();
   }
   async editAnArticle() {
     await this.editArticle.click();
@@ -25,5 +27,8 @@ export class NewArticlePage extends BasePage {
   }
   async postTheComment() {
     await this.postAcomment.click();
+  }
+  async giveAHeartForTheArticle() {
+    await this.likeTheArticle.click();
   }
 }
--- tests/add-article-tests.spec.ts
+++ tests/add-article-tests.spec.ts
@@ -38,8 +38,10 @@ test.describe("new article", () => {
     await expect(addNewArticlePage.articleTitleInput).toBeVisible();
   });
   test("user is able to write and post a comment", async ({ page }) => {
+    const uniqueTitle = `Playwright ${Date.now()}`;
+
     await addNewArticlePage.openNewArticleLink();
-    await addNewArticlePage.completeArticleTitle("Playwright");
+    await addNewArticlePage.completeArticleTitle(uniqueTitle);
     await addNewArticlePage.completeArticleDescription("About Playwright");
     await addNewArticlePage.completeArticleTextBody(
       "Playwright is an amaizing automation tool",
@@ -47,8 +49,78 @@ test.describe("new article", () => {
     await addNewArticlePage.completeTagsArticle("Playwright");
     await addNewArticlePage.publishArticle();
 
-    await expect(addNewArticlePage.articleTitleInput).toBeVisible();
+    await expect(page).toHaveURL(/article/);
+
     await newArticlePage.AddAComment("Playwright is more than awesome!");
     await newArticlePage.postTheComment();
+
+    await expect(
+      page.getByText("Playwright is more than awesome!"),
+    ).toBeVisible();
+  });
+  test("user is able to edit the article", async ({ page }) => {
+    const uniqueTitle = `Playwright ${Date.now()}`;
+
+    await addNewArticlePage.openNewArticleLink();
+    await addNewArticlePage.completeArticleTitle(uniqueTitle);
+    await addNewArticlePage.completeArticleDescription("About Playwright");
+    await addNewArticlePage.completeArticleTextBody(
+      "Playwright is an amaizing automation tool",
+    );
+    await addNewArticlePage.completeTagsArticle("Playwright");
+    await addNewArticlePage.publishArticle();
+
+    await expect(page).toHaveURL(/article/);
+
+    await newArticlePage.editAnArticle();
+    await addNewArticlePage.completeArticleDescription(
+      "About Playwright and automation tools",
+    );
+    await addNewArticlePage.completeArticleTextBody(
+      "Playwright is an amaizing and useful automation tool",
+    );
+    await addNewArticlePage.publishArticle();
+
+    await expect(page.locator(".row.article-content")).toHaveText(
+      "Playwright is an amaizing and useful automation tool",
+    );
+  });
+  test("user is able to delete the article", async ({ page }) => {
+    const uniqueTitle = `Playwright ${Date.now()}`;
+
+    await addNewArticlePage.openNewArticleLink();
+    await addNewArticlePage.completeArticleTitle(uniqueTitle);
+    await addNewArticlePage.completeArticleDescription("About Playwright");
+    await addNewArticlePage.completeArticleTextBody(
+      "Playwright is an amaizing automation tool",
+    );
+    await addNewArticlePage.completeTagsArticle("Playwright");
+    await addNewArticlePage.publishArticle();
+
+    await expect(page).toHaveURL(/article/);
+
+    await newArticlePage.deleteAnArticle();
+
+    await expect(page).toHaveURL("/");
+  });
+  test("user is able to like the article", async ({ page }) => {
+    const uniqueTitle = `Playwright ${Date.now()}`;
+
+    await addNewArticlePage.openNewArticleLink();
+    await addNewArticlePage.completeArticleTitle(uniqueTitle);
+    await addNewArticlePage.completeArticleDescription("About Playwright");
+    await addNewArticlePage.completeArticleTextBody(
+      "Playwright is an amaizing automation tool",
+    );
+    await addNewArticlePage.completeTagsArticle("Playwright");
+    await addNewArticlePage.publishArticle();
+
+    await expect(page).toHaveURL(/article/);
+
+    await page.getByText("Home").click();
+
+    await newArticlePage.giveAHeartForTheArticle();
+
+    await expect(newArticlePage.likeTheArticle).toHaveCount(1);
   });
 });
